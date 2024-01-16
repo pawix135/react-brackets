f8e684881bcc8018ae2b86cdfef39712d9a9781a
pawix135 <leagueoflegendsoppl@gmail.com>
2024-01-16 08:13:18 +0100

    updated to react@18, removed swipable-views

--- src/brackets/index.tsx
+++ src/brackets/index.tsx
@@ -1,7 +1,5 @@
 import React, { Fragment } from 'react';
 import { Round, Bracket, SeedsList } from '../components/round';
-import SwipeableViews from 'react-swipeable-views';
-import useMedia from '../hooks/useMedia';
 import { renderSeed, renderTitle } from '../utils/renders';
 import { ISingleEliminationProps } from '../types/SingleElimination';
 
@@ -10,14 +8,10 @@ const SingleElimination = ({
   rtl = false,
   roundClassName,
   bracketClassName,
-  swipeableProps = {},
   mobileBreakpoint = 992,
   renderSeedComponent = renderSeed,
   roundTitleComponent = renderTitle,
 }: ISingleEliminationProps) => {
-  // Checking responsive size
-  const isResponsive = useMedia(mobileBreakpoint);
-
   const data = rounds.map((round, roundIdx) => (
     <Round key={round.title} className={roundClassName} mobileBreakpoint={mobileBreakpoint}>
       {round.title && roundTitleComponent(round.title, roundIdx)}
@@ -39,17 +33,6 @@ const SingleElimination = ({
     </Round>
   ));
 
-  if (isResponsive) {
-    // Since SwipeableViewsProps have an issue that it uses ref inside of it, We need to remove ref from the object
-    const { ref, ...rest } = swipeableProps;
-    return (
-      <Bracket className={bracketClassName} dir={rtl ? 'rtl' : 'ltr'} mobileBreakpoint={mobileBreakpoint}>
-        <SwipeableViews style={{ minHeight: '500px' }} axis={rtl ? 'x-reverse' : 'x'} {...rest}>
-          {data}
-        </SwipeableViews>
-      </Bracket>
-    );
-  }
   return (
     <Bracket className={bracketClassName} dir={rtl ? 'rtl' : 'ltr'} mobileBreakpoint={mobileBreakpoint}>
       {data}
